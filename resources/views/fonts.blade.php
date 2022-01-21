efc12c02f379d0ad28ab618de3ed65d120c8d6c7
Koramit Pichanaharee <koramit.p@gmail.com>
2022-01-21 19:47:22 +0700

    font selection

---
+++ resources/views/fonts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>เลือก font</title>
+    <style>
+        li {
+            margin: 1rem 0 1rem 0;
+        }
+    </style>
+</head>
+<body>
+    <h1>รายการฟอนต์</h1>
+
+    <ol>
+        <a href="{{ url('/fonts/bai-jamjuree') }}"><li>ฟอนต์ Bai Jamjuree click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/ibm') }}"><li>ฟอนต์ IBM Plex Sans Thai click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/k2d') }}"><li>ฟอนต์ K2D click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/krub') }}"><li>ฟอนต์ Krub click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/mitree') }}"><li>ฟอนต์ Mitree click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/niramit') }}"><li>ฟอนต์ Niramit click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/prompt') }}"><li>ฟอนต์ Prompt click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/sarabun') }}"><li>ฟอนต์ Sarabun click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/taviraj') }}"><li>ฟอนต์ Taviraj click เพื่อดูตัวอย่าง</li></a>
+        <a href="{{ url('/fonts/trirong') }}"><li>ฟอนต์ Trirong click เพื่อดูตัวอย่าง</li></a>
+    </ol>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/fonts', function () {
+    return view('fonts');
+});
+
+Route::get('/fonts/{font}', function ($font) {
+    return view('font-sample', ['fontName' => $font]);
+});
